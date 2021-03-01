f316dce720f724d5c4e28819c5f15605397dd1e2
Anna Kornilova <67694230+kornilovaap@users.noreply.github.com>
2021-03-01 14:52:53 +0300

    for in

--- Lesson_2/lessons/for-in.py
+++ Lesson_2/lessons/for-in.py
@@ -1,22 +1,38 @@
 name = ["John", "Jack", "Kate"]  # СПИСОК
 
-# index = 0
-# while index < len(name):  # len работает и для СТРОК тоже
-#     print(f"Hello, {name[index]}")
-#     index += 1
+index = 0
+while index < len(name):  # len работает и для СТРОК тоже
+    print(f"Hello, {name[index]}")
+    index += 1
 
-# for user in name:
-#     print(f"Hello, {user}")
-#
-# person = {
-#     "name": "John",
-#     "age": 10}
-#
-# for key, value in person.items():  # items берет сразу все значения из "строки"
-#     print(f"Key {key} = {value}")
+for user in name:
+    print(f"Hi, {user}")
 
-for i in range(3):
-    for j in range(5):
+person = {
+    "name": "John",
+    "age": 10}
+
+for key, value in person.items():  # items берет сразу все значения из "строки"
+    print(f"{key} - {value}")
+
+for i in range(1, 4):
+    for j in range(1, 6):
         if j > i:
             break
         print(i, j)
+_______________________
+Hello, John
+Hello, Jack
+Hello, Kate
+Hi, John
+Hi, Jack
+Hi, Kate
+name - John
+age - 10
+1 1
+2 1
+2 2
+3 1
+3 2
+3 3
+ 
