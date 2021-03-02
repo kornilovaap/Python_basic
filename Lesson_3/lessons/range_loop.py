6804c637174f604c286fe2a558644d9f5d48a01b
Anna Kornilova <67694230+kornilovaap@users.noreply.github.com>
2021-03-02 13:31:04 +0300

    range()

--- Lesson_3/lessons/range_loop.py
+++ Lesson_3/lessons/range_loop.py
@@ -1,13 +1,27 @@
 i = 0
 
-while i < 10:
+while i < 5:
     print(i)
     i += 1
 
 # аналогичная запись
-for el in range(10, 20, 2):
+for el in range(40, 50, 2):
     print(el)
 
 
 print(range(5))
 print(list(range(5)))
+____________________________
+0
+1
+2
+3
+4
+40
+42
+44
+46
+48
+range(0, 5)
+[0, 1, 2, 3, 4]
+ 
