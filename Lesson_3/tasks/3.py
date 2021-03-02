6ffc78674cde56bedf160839b81a5facac627030
Anna Kornilova <67694230+kornilovaap@users.noreply.github.com>
2021-03-02 16:10:12 +0300

    def(), lambda, max()
    несколько вариантов решения

--- Lesson_3/tasks/3.py
+++ Lesson_3/tasks/3.py
@@ -8,4 +8,19 @@ def my_func(*args):
     return sum(args) - min(args)
 
 
+def my_func_2(a, b, c):
+    return max(a + b, b + c, c + a)
+
+
+# lambda
+my_func_lambda = lambda a, b, c: max(a + b, b + c, c + a)
+
+
 print(my_func(1, 2, -3))
+print(my_func_2(-1, 2, 0))
+print(my_func_lambda(5, -5, 4))
+______________________
+3
+2
+9
+ 
