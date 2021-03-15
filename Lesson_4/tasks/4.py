1204b0dad09815aad2b950da6fcd3dcd658c096f
Anna Kornilova <67694230+kornilovaap@users.noreply.github.com>
2021-03-15 19:44:37 +0300

    Update 4.py

--- Lesson_4/tasks/4.py
+++ Lesson_4/tasks/4.py
@@ -11,3 +11,6 @@ my_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 
 new_list = [x for x in my_list if my_list.count(x) == 1]
 print(new_list)
+_____________________________
+[23, 1, 3, 10, 4, 11]
+    
