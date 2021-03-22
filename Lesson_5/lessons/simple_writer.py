35b3b32ed385955b4c8af6dee2cf333d7ee7637b
Anna Kornilova <67694230+kornilovaap@users.noreply.github.com>
2021-03-22 17:36:42 +0300

    Add files via upload

---
+++ Lesson_5/lessons/simple_writer.py
@@ -0,0 +1,11 @@
+my_file = open(r'data.txt', "w")  # raw
+
+if my_file.writable():
+    my_file.write("Update\n")
+
+    strings = ["Anna\n", "Roma\n"]
+    my_file.writelines(strings)
+else:
+    print("Can not write")
+
+my_file.close()
