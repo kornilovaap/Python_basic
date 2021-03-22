21d4026ee5b4a9985af34ca0bea957a5c972c612
Anna Kornilova <67694230+kornilovaap@users.noreply.github.com>
2021-03-22 17:45:09 +0300

    Add files via upload

---
+++ Lesson_5/lessons/context_reader.py
@@ -0,0 +1,13 @@
+with open('data.txt') as my_file:
+    print(my_file.read())
+
+print(type(my_file))  # <class '_io.TextIOWrapper'>
+# print(my_file.read())  # ValueError: I/O operation on closed file, т.е. файл уже закрылся
+
+
+# Exception handler
+try:
+    with open('data.txt') as my_file:
+        print(my_file.read())
+except IOError:
+    print("Some error")
