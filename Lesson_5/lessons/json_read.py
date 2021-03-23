246e95e9c6b74963b8f969f7d47f437458131daa
Anna Kornilova <67694230+kornilovaap@users.noreply.github.com>
2021-03-23 13:43:20 +0300

    Add files via upload

---
+++ Lesson_5/lessons/json_read.py
@@ -0,0 +1,10 @@
+import json
+
+with open('person.json') as file_stream:
+    person = json.load(file_stream)
+
+    print(person)  # {'name': 'John', 'age': 40, 'salaries': [100, 200, 160]}
+    print(sum(person['salaries']))  # 460
+
+print(json.loads('{"name": "John", "age": 40, "salaries": [100, 200, 160]}'))
+# {'name': 'John', 'age': 40, 'salaries': [100, 200, 160]}
